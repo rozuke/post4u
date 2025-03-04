88ad30ef685d320696c710dba04d1239ad788460
rozuke <rodrigo.sulca499@gmail.com>
2025-03-04 12:22:43 -0400

    Add pagination with scroll

--- src/app/features/feed/feed.component.ts
+++ src/app/features/feed/feed.component.ts
@@ -1,23 +1,98 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { PostCardComponent } from '../../shared/components/post-card/post-card.component';
 import { PostService } from '../../core/services/post.service';
 import { PostResponse } from '../../shared/models/api/post.model';
 import { InitialsPipe } from '../../shared/pipes/initials.pipe';
 import { TimeAgoPipe } from '../../shared/pipes/time-ago.pipe';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-feed',
-  imports: [PostCardComponent, InitialsPipe, TimeAgoPipe],
+  imports: [
+    PostCardComponent,
+    InitialsPipe,
+    TimeAgoPipe,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css',
 })
 export class FeedComponent implements OnInit {
   private readonly postService = inject(PostService);
+  private readonly snackBar = inject(MatSnackBar);
 
-  public data: PostResponse[] = [];
+  public allPosts: PostResponse[] = [];
+  public displayedPosts: PostResponse[] = [];
+  public loading = false;
+  public hasMore = true;
+
+  private currentIndex = 0;
+  private pageSize = 10;
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe(posts => {
-      this.data = posts.reverse();
+    this.loadInitialPosts();
+  }
+
+  private loadInitialPosts(): void {
+    this.loading = true;
+    this.postService.getPosts().subscribe({
+      next: posts => {
+        console.log('Total  posts: ', posts.length);
+        this.allPosts = posts.reverse();
+        this.loadMorePosts();
+        this.loading = false;
+      },
+      complete: () => (this.loading = false),
     });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  @HostListener('document:mousewheel', ['$event'])
+  onScrollOrMousewheel(): void {
+    const threshold = 100;
+    const position = window.scrollY + window.innerHeight;
+    const height = document.body.offsetHeight;
+
+    if (position > height - threshold && this.hasMore && !this.loading) {
+      this.loading = true;
+      setTimeout(() => {
+        this.loadMorePosts();
+      }, 4000);
+    }
+  }
+
+  private loadMorePosts(): void {
+    console.log('Loading more posts');
+    if (this.currentIndex >= this.allPosts.length) {
+      this.hasMore = false;
+      this.showNoMorePostsMessage();
+      return;
+    }
+
+    const nextPosts = this.allPosts.slice(
+      this.currentIndex,
+      this.currentIndex + this.pageSize
+    );
+
+    this.displayedPosts = [...this.displayedPosts, ...nextPosts];
+    this.currentIndex += this.pageSize;
+    if (this.currentIndex >= this.allPosts.length) {
+      this.hasMore = false;
+      this.showNoMorePostsMessage();
+    }
+
+    this.loading = false;
+  }
+
+  private showNoMorePostsMessage(): void {
+    this.snackBar.open(
+      'There are not more posts to show, reload the page',
+      'Cerrar',
+      {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      }
+    );
   }
 }
